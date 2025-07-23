c6b1b8175a480e6533a7f0d7eb6a2dfb4709eff1
Corvin <corvinszimion@googlemail.com>
2025-07-23 16:02:47 +0200

    added settings viewmodel

---
+++ SkyFlipR/Features/Settings/SettingsView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SkyFlipR.Features.Settings;
+
+/// <summary>
+/// Interaction logic for SettingsView.xaml
+/// </summary>
+public partial class SettingsView : UserControl
+{
+    public SettingsView() : this(App.AppServices.GetRequiredService<SettingsViewModel>())
+    {
+        InitializeComponent();
+    }
+
+    public SettingsView(SettingsViewModel viewModel)
+    {
+        InitializeComponent();
+        DataContext = viewModel;
+    }
+}
---
+++ SkyFlipR/Features/Settings/SettingsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using MaterialDesignThemes.Wpf;
+
+using SkyFlipR.Features.ReleaseNotes;
+using SkyFlipR.Services;
+using SkyFlipR.Services.ErrorHandling;
+
+using Velopack;
+using Velopack.Sources;
+
+namespace SkyFlipR.Features.Settings;
+
+public partial class SettingsViewModel : ObservableObject
+{
+    private readonly ISnackbarMessageQueue _snackbarMessageQueue;
+    private readonly IThemeService _themeService;
+    private readonly IErrorHandler _errorHandler;
+
+    public SettingsViewModel(ISnackbarMessageQueue snackbarMessageService,
+                             IThemeService themeService,
+                             IErrorHandler errorHandler,
+                             VelopackUpdaterViewModel velopackUpdater)
+    {
+        _snackbarMessageQueue = snackbarMessageService;
+        _themeService = themeService;
+        _errorHandler = errorHandler;
+        VelopackUpdater = velopackUpdater;
+        SelectedTheme = ThemeOptions.First();
+    }
+
+    public BaseTheme[] ThemeOptions { get; } = Enum.GetValues<BaseTheme>();
+    public VelopackUpdaterViewModel VelopackUpdater { get; }
+
+    [ObservableProperty]
+    private BaseTheme _selectedTheme;
+
+    partial void OnSelectedThemeChanged(BaseTheme value)
+    {
+        _themeService.SetTheme(value);
+    }
+}
